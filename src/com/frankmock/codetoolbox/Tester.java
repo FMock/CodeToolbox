67a763897d0cae31808deccfc9420cdcd48f1ef4
FMock <FMock@users.noreply.github.com>
2017-05-31 17:28:27 -0700

    Use Tester to test each classes methods

---
+++ src/com/frankmock/codetoolbox/Tester.java
@@ -0,0 +1,15 @@
+package com.frankmock.codetoolbox;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		NumericTools nt = new NumericTools();
+		
+		int[] digits = nt.decomposeInteger(12345);
+		
+		for(int i : digits){
+			System.out.print(i + System.getProperty("line.separator"));
+		}
+	}
+
+}
