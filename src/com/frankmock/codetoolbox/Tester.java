1c4d7917f414094f1b2836202bf7f2497f318e17
FMock <FMock@users.noreply.github.com>
2017-06-01 10:38:25 -0700

    Searching class added, improved decomposeInteger

--- src/com/frankmock/codetoolbox/Tester.java
+++ src/com/frankmock/codetoolbox/Tester.java
@@ -5,11 +5,14 @@ public class Tester {
 	public static void main(String[] args) {
 		NumericTools nt = new NumericTools();
 		
-		int[] digits = nt.decomposeInteger(12345);
+		int[] digits = nt.decomposeInteger(-111);
 		
 		for(int i : digits){
 			System.out.print(i + " ");
 		}
+		
+		int ei = Searching.EquilibriumIndex(digits);
+		System.out.println("The equilibrium index is " + ei);
 	}
 
 }
